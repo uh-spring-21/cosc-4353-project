26942175cd0b381e4ce8940dd90f0d9f91ea720e
themartinlopez <code.mlopez@gmail.com>
2021-03-18 12:51:50 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -19,6 +19,10 @@ app.use("/auth", require("./routes/jwtAuth"));
 //fuel quote form and order history and pricing module
 app.use("/order", require("./routes/quoteOrders"));
 
+//client profile
+app.use("/clientInformation", require("./routes/clientInformation"));
+
+
 app.listen(5000,()=>{
     console.log("server has started");
 });
